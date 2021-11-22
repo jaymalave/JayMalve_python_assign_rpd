df2930e6447ac28e42240d42c6f8e1f2a7135726
jaymalave <jaym.github@gmail.com>
2021-11-23 00:08:58 +0530

    minor refactor

--- task1/script.py
+++ task1/script.py
@@ -10,19 +10,24 @@ from selenium.webdriver.common.keys import Keys
 #function to load env variables
 load_dotenv()
 
+
 #get a particular env variable and store in a variable
 login_id = os.environ.get("LOGIN_ID")
 password = os.environ.get("PASSWORD")
+
 
 #connect to the chromedriver installed in the system
 chromedriver = '/usr/local/bin/chromedriver'
 browser = webdriver.Chrome(chromedriver)
 
+
 #with the help of webdriver, open the required link
 browser.get(('https://www.zacks.com/screening/stock-screener?icid=screening-screening-nav_tracking-zcom-main_menu_wrapper-stock_screener'))
 
+
 #wait until the page is loaded completely
 browser.implicitly_wait(20) 
+
 
 #get control of the element on the webpage using find_element() and perform the
 #requied operations using the built in selenium functions
@@ -32,22 +37,27 @@ myScreenButton.click()
 #explicit sleeps added so that the script does not crash when the webpage is loading
 sleep(3)
 
+
 loginRef = browser.find_element(By.XPATH, '/html/body/main/section/div/div[8]/section/div/div/div/section[1]/form/div[1]/input')
 loginRef.send_keys(login_id) #pass login_id to the loginRef input element
 passWordRef = browser.find_element(By.XPATH, '/html/body/main/section/div/div[8]/section/div/div/div/section[1]/form/div[2]/input')
 passWordRef.send_keys(password) #pass password to the passwordRef input element
 
+
 signInButton = browser.find_element(By.XPATH, '/html/body/main/section/div/div[8]/section/div/div/div/section[1]/form/button')
 signInButton.click()
 sleep(5)
+
 
 runButton = browser.find_element(By.XPATH, '/html/body/main/section/div/div[6]/section/div/div/div/div/table/tbody/tr/td[3]/a[1]')
 runButton.click()
 sleep(2)
 
+
 selectAllTickers = browser.find_element(By.XPATH, '/html/body/main/section/div/div[4]/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/table/thead/tr/th[1]/input')
 selectAllTickers.click()
 sleep(5)
+
 
 csvButton = browser.find_element(By.XPATH, '/html/body/main/section/div/div[4]/div/div[1]/div[2]/div[1]/div/div[1]/a[1]')
 csvButton.click()
--- task2/script.py
+++ task2/script.py
@@ -8,37 +8,47 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+
 #function to load env variables
 load_dotenv()
+
 
 #connect to the chromedriver installed in the system
 chromedriver = '/usr/local/bin/chromedriver'
 browser = webdriver.Chrome(chromedriver)
 
+
 #with the help of webdriver, open the required link
 browser.get(('https://www.zacks.com/funds/etf/veu/holding'))
+
 
 #read the csv file(in the same directory) using read_csv() function in pandas
 df = pd.read_csv('data.csv')
 
+
 #access the first column in the csv file and save the elements in the column in a list
 ticker_list = df.iloc[:, 0].to_list()
+
 
 #traverse through every ticker in the list and perform the required scraping operation
 for ticker in ticker_list:
     browser.refresh() #refresh the page after every ticker scraping operation
     sleep(20)
+
     #get control of the element on the webpage using find_element() and perform the
     #requied operations using the built in selenium functions
+
     searchRef = browser.find_element(By.CSS_SELECTOR, '#ticker') 
     #transform ticker into all uppercase and pass as an input to searchRef
     searchRef.send_keys(ticker.upper()) 
     searchRef.submit()  #selenium function to press enter
     sleep(3)
   
+
     #get access to the table
     table = browser.find_element(By.CSS_SELECTOR, "#etf_holding_table")
     #create a new csv file named after the corresponding ticker and write to it
+    
     with open(ticker + '.csv', 'w', newline='') as csvfile:
         #use writer() from 'csv' to write to the file
         wr = csv.writer(csvfile)
